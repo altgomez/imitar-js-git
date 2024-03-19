fc20c0f6f45f8e4b8eb8949c7dce29e3e4a45f67
yesid <altgomez75@gmail.com>
2024-03-19 12:58:27 +0100

    refactor: refactorize javascript functions

--- main.js
+++ main.js
@@ -1,25 +1,16 @@
-function changeColorblue(from_color, to_color) {
+function changeColor(event) {
+    const button = event.target
+    const from_color = button.attributes.from_color.value
+    const to_color = button.attributes.to_color.value
+
     const square = document.getElementById('square')
     square.classList.remove(from_color)
     square.classList.add(to_color)
 }
 
-function changeColorToBlue(event) {
-    const button = event.target
-    const from_color = button.attributes.from_color.value
-    const to_color = button.attributes.to_color.value
-    changeColor(from_color, to_color)
-}
-
-function changeColorToRed(event) {
-    const button = event.target
-    const from_color = button.attributes.from_color.value
-    const to_color = button.attributes.to_color.value
-    changeColor(from_color, to_color)
-}
-
 const blue_button = document.getElementById('blue_button')
-blue_button.onclick = changeColorToBlue
+lue_button.onclick = changeColor
 
 const red_button = document.getElementById('red_button')
-red_button.onclick = changeColorToRed
+red_button.onclick = changeColor
+
